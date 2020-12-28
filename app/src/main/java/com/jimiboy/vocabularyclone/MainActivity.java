b2f4b7727ffd5a539cf5f8c04271c2337ba7f366
yorusicu <yorusicu@gmail.com>
2020-12-29 00:08:31 +0900

    로그인 회원가입 엑티비티 완료, 로그인 > 회원가입 이동, 회원가입에서 join.do로 이동해 로컬DB에 데이터입력 완료

--- app/src/main/java/com/jimiboy/vocabularyclone/MainActivity.java
+++ app/src/main/java/com/jimiboy/vocabularyclone/MainActivity.java
@@ -2,13 +2,41 @@ package com.jimiboy.vocabularyclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+EditText etId, etPw;
+Button btnLogin, btnJoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etId=findViewById(R.id.et_log_id);
+        etPw=findViewById(R.id.et_log_pw);
+        btnLogin=findViewById(R.id.btn_log_login);
+        btnJoin=findViewById(R.id.btn_log_join);
+
+        btnLogin.setOnClickListener(this);
+        btnJoin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_log_login:
+
+                break;
+            case R.id.btn_log_join:
+                Intent intentJoin = new Intent(this,com.jimiboy.vocabularyclone.MainActivity_join.class );
+                startActivity(intentJoin);
+                break;
+
+        }
     }
 }
